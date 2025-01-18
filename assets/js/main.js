992e0652dbe2f6f9d4ab2063335ef5a5c42ee543
sribalaji-sp <sribalaji.vnb@gmail.com>
2025-01-18 10:05:00 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*======= Menu Show ========*/
+/* validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*======= Menu Hidden =======*/
+/* validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const LinkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click' , LinkAction))
+
+/*=============== SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+} 
+window.addEventListener('scroll' , shadowHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+const scrollup = () =>{
+    const scrollup = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollup.classList.add('show-scroll')
+                        : scrollup.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollup)
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section[id]')
+
+    const scrollActive = () => {
+        const scrollY = window.scrollY
+
+        sections.forEach(current => {
+            const sectionHeight = current.offsetHeight,
+                  sectionTop = current.offsetTop - 58,
+                  sectionId = current.getAttribute('id'),
+                  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+            if (sectionsClass) { // Check if sectionsClass is not null
+                if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                    sectionsClass.classList.add('active-link')
+                } else {
+                    sectionsClass.classList.remove('active-link')
+                }
+            }
+        })
+    }
+
+    window.addEventListener('scroll', scrollActive)
+})
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// previously selected topic
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-Icon')
+
+//we obtain the current theme 
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+//we validate if the user previously chose a topic 
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+//Active or deactive the theme with button
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme' , getCurrentTheme())
+    localStorage.setItem('selected-icon' , getCurrentIcon())
+})
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+
+})
+
+sr.reveal('.home__data, .join__container, .footer')
+sr.reveal('.home__img', {origin: 'bottom'})
+sr.reveal('.enjoy__card, .popular__card ', {interval: 100})
+sr.reveal('.about__data', {origin: 'right'})
+sr.reveal('.about__img', {origin: 'left'})
